1e730e5d915dd68fa221d277957e8fef6d292924
Raira <rairasimoes@gmail.com>
2021-03-23 17:33:02 -0300

    V. 0.0.1 - structural update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { DoctorsService } from './services/doctors/doctors.service';
+import { HtmlService } from './services/html/html.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,100 @@ import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular
 })
 export class AppComponent {
   title = 'oowlish';
+  doctors
+  doctorsList = []
+  doctorsElements
+
+  constructor(
+    private elRef: ElementRef,
+    private doctorsService: DoctorsService,
+    public htmlService: HtmlService
+  ) { }
+
+  ngOnInit(): void {
+    this.appendSearchBar()
+  }
+
+  ngAfterViewInit() {
+    this.getHtmlElements()
+    this.getDoctorFilteredList('all')
+    for (let i = 0; i < this.doctorsElements.length; i++)
+      this.doctorsList[this.doctorsElements[i].dataset.upin] = this.doctorsElements[i];
+  }
+
+
+  getDoctorStatus() {
+    for (let i = 0; i < this.doctors.length; i++) {
+      this.doctorsElements[i].children[3].innerHTML = !this.doctors[i].available ? this.htmlService.unavailableButton : this.htmlService.availableButton
+    }
+  }
+
+  async getSelectedDoctor(upin: any) {
+    let doctor = this.doctors.filter(doc => { return doc.upin.toString() === upin })[0]
+    if (doctor) {
+      doctor.available = !doctor.available
+      this.getDoctorStatus()
+      await this.doctorsService.changeDisponibility(doctor)
+    }
+  }
+
+  async getDoctorFilteredList(filter: string) {
+    this.doctors = await this.doctorsService.getDoctors()
+    if (filter === 'all') this.showAllDoctors();
+    else this.getAvailableDoctors();
+    this.getDoctorStatus()
+  }
+
+  showAllDoctors() {
+    for (let doctor of this.doctorsElements)
+      doctor.hidden = false
+  }
+
+  getAvailableDoctors() {
+    this.doctors.forEach((doctor) => {
+      if (!doctor.available)
+        for (let doctorUpin in this.doctorsList)
+          if (doctorUpin === doctor.upin.toString())
+            this.doctorsList[doctorUpin].hidden = true
+    })
+  }
+
+  getHtmlElements() {
+    let filter = this.elRef.nativeElement.querySelector('#availabilityFilterSelect')
+    this.doctorsElements = this.htmlService.getDoctorsElements().doctors
+    let table = this.htmlService.getDoctorsElements().table
+
+    table.addEventListener("click", (e: any) => {
+      if (e.target.toString() == '[object HTMLButtonElement]') {
+        this.getSelectedDoctor(e.path[2].dataset.upin)
+      }
+    })
+    filter.onchange = () => {
+      this.getDoctorFilteredList(filter.value)
+    }
+  }
+
+  appendSearchBar() {
+    let searchbar = this.htmlService.createSearchbarElement()
+    searchbar.onkeyup = () => {
+      var searchTerm = (<HTMLInputElement>document.getElementById("searchBar")).value;
+      this.search(searchTerm)
+    }
+    document.getElementById('searchContainer').appendChild(searchbar);
+  }
+
+  search(searchTerm: string) {
+    for (let doctor of this.doctors) {
+      const doctorName = doctor.name.toLowerCase()
+      const doctorUpin = doctor.upin.toString()
+      if ((doctorName.includes(searchTerm.toLowerCase()) || doctorUpin.includes(searchTerm)))
+        this.doctorsList[doctor.upin].hidden = false
+      else {
+        this.doctorsList[doctor.upin].hidden = true
+      }
+    }
+  }
 }
+
+
+
--- src/app/services/html/html.service.spec.ts
+++ src/app/services/html/html.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { HtmlService } from './html.service';
@@ -6,7 +7,9 @@ describe('HtmlService', () => {
   let service: HtmlService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(HtmlService);
   });
 
